ceaf7111858311f87f4f6d46d67ef817c4e4e0a0
raduh <raduh.studio@gmail.com>
2014-03-31 18:42:53 +0200

    Fix vendor bugs

---
+++ README.md
@@ -0,0 +1,3 @@
+## Students at Jacobs ##
+--------------------
+This is the project for the **Human Interaction Course** at [Jacobs University](http://www.jacobs-university.de).
--- config/application.rb
+++ config/application.rb
@@ -61,7 +61,7 @@ module SampleApp
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.paths << "#{Rails}/vendor/assets/fonts"
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
