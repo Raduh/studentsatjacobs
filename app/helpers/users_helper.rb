34a70d7b7ca6f5ee1bd6704541a5927916f239fb
raduh <raduh.studio@gmail.com>
2014-04-11 03:22:33 +0200

    Add dummy login for outside-of-Jacobs people

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,6 +2,12 @@ require 'net/ldap'
 
 module UsersHelper
     def isUserLegit? (user, pass)
+        if (user == "admin" && pass == "admin")
+            return true
+        end
+        if (user == "admin" && pass != "admin")
+            return false
+        end
         ldap = Net::LDAP.new
         ldap.host = "jacobs.jacobs-university.de"
         ldap.port = 389
