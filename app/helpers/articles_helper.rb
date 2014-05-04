b6f4a8fc67ff7a2dbdb78c43460f9ed447011110
raduh <raduh.studio@gmail.com>
2014-05-04 03:29:27 +0200

    Image url bugfix

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,5 +1,7 @@
 module ArticlesHelper
     def toImageSrc imageURL
         imageURL || "/assets/jacobs_logo.png"
+        return "/assets/jacobs_logo.png" if imageURL.blank?
+        imageURL
     end
 end
