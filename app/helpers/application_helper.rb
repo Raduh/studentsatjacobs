4153647b3722b37db10105c228f857029d8c5e00
raduh <raduh.studio@gmail.com>
2014-05-04 03:37:25 +0200

    Speed up page loading

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,11 @@
 module ApplicationHelper
     #Returns the full title on a per-page basis
     def full_title(page_title)
+        @location = page_title
         base_title = "Students @ Jacobs"
         if page_title.empty?
             base_title
+            @location = ""
         else
             "#{base_title} | #{page_title}"
         end
