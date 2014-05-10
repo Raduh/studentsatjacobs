83b65ef154a2b8bdd2dbfe017e0b7263e76caee6
raduh <raduh.studio@gmail.com>
2014-05-10 18:27:16 +0200

    Add date label to calendar

--- app/assets/javascripts/controls.js
+++ app/assets/javascripts/controls.js
@@ -22,7 +22,7 @@
 			});
 		},
 		onAfterViewLoad: function(view) {
-			$('.page-header h3').text(this.getTitle());
+			$('.jDateLabel h3').text(this.getTitle());
 			$('.btn-group button').removeClass('active');
 			$('button[data-calendar-view="' + view + '"]').addClass('active');
 		},
