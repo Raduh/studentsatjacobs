f8ad7666200d54267d2f003ec03769b90e534e42
raduh <raduh.studio@gmail.com>
2014-05-11 20:54:36 +0200

    Add events to calendar

---
+++ public/events.json.php
@@ -0,0 +1,77 @@
+{
+    "success": 1,
+    "result": [
+        {
+            "id": "293",
+            "title": "Amazing Indian CID",
+            "url": "#",
+            "class": "event-success",
+            "start": "1401645600000",
+            "end":   "1401652800000"
+        },
+        {
+            "id": "256",
+            "title": "Barbeque campus green",
+            "url": "#",
+            "class": "event-success",
+            "start": "1400594400000",
+            "end":   "1400601600000"
+        },
+        {
+            "id": "276",
+            "title": "Cheerleading demo",
+            "url": "#",
+            "class": "event-info",
+            "start": "1400356800000",
+            "end":   "1400358600000"
+        },
+        {
+            "id": "294",
+            "title": "Hanse Boat Race",
+            "url": "#",
+            "class": "event-info",
+            "start": "1399136400000",
+            "end":   "1399147200000"
+        },
+        {
+            "id": "297",
+            "title": "J-Cappella Concert",
+            "url": "#",
+            "class": "event-info",
+            "start": "1401458400000",
+            "end":   "1401465600000"
+        },
+        {
+            "id": "54",
+            "title": "Graduation Ceremony",
+            "url": "#",
+            "class": "event-important",
+            "start": "1402048800000",
+            "end":   "1402056000000"
+        },
+        {
+            "id": "532",
+            "title": "Pancake cheer-up",
+            "url": "#",
+            "class": "event-success",
+            "start": "1400608800000",
+            "end":   "1400616000000"
+        },
+        {
+            "id": "548",
+            "title": "Summer holiday starts",
+            "url": "#",
+            "class": "event-important",
+            "start": "1402315200000",
+            "end":   "1402315200000"
+        },
+        {
+            "id": "295",
+            "title": "Flea market",
+            "url": "#",
+            "class": "event-special",
+            "start": "1397995200000",
+            "end":   "1398002400000"
+        }
+    ]
+}
